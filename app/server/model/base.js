4e5cf3009e5d3d36c9f5e2192fdc5caf9d34dbba
leonardo <wang861622836@163.com>
2017-07-06 15:03:59 +0800

    update mysql 的线上密码获取方式

--- app/server/model/base.js
+++ app/server/model/base.js
@@ -1,12 +1,16 @@
 'use strict';
 //ef009d524454
 const Sequelize = require('sequelize');
-var passWord = '';
-if (process.env.NODE_ENV == 'prod') {
-  passWord = '104ef09afb';
+const fs = require('fs');
+const configPath = __dirname + '/../../../config.json';
+
+var password = '';
+if (process.env.NODE_ENV == 'prod' && fs.existsSync(configPath)) {
+  const _config = fs.readFileSync(configPath ,'UTF-8');
+  password = _config.mysql.password || password;
 }
 const db = {
-  sequelize: new Sequelize('code', 'root', passWord, {
+  sequelize: new Sequelize('code', 'root', password, {
     timezone: '+08:00',
     define: {
       freezeTableName: true,
